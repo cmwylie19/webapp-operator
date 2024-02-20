ad163f04a21b9d33e4298d369fdd108fabf8bcb7
Case Wylie <cmwylie19@defenseunicorns.com>
2024-02-20 17:34:42 -0500

    validations
    Signed-off-by: Case Wylie <cmwylie19@defenseunicorns.com>

--- capabilities/crd/generated/webapp-v1alpha1.ts
+++ capabilities/crd/generated/webapp-v1alpha1.ts
@@ -3,53 +3,50 @@
 import { GenericKind, RegisterKind } from "kubernetes-fluent-client";
 
 export class WebApp extends GenericKind {
-    apiVersion?: string;
-    kind?:       string;
-    metadata?:   { [key: string]: any };
-    spec?:       Spec;
-    status?:     Status;
+  spec?: Spec;
+  status?: Status;
 }
 
 export interface Spec {
-    /**
-     * Language defines the language of the web application, either English (en) or Spanish (es).
-     */
-    language: Language;
-    /**
-     * Replicas is the number of desired replicas.
-     */
-    replicas: number;
-    /**
-     * Theme defines the theme of the web application, either dark or light.
-     */
-    theme: Theme;
+  /**
+   * Language defines the language of the web application, either English (en) or Spanish (es).
+   */
+  language: Language;
+  /**
+   * Replicas is the number of desired replicas.
+   */
+  replicas: number;
+  /**
+   * Theme defines the theme of the web application, either dark or light.
+   */
+  theme: Theme;
 }
 
 /**
  * Language defines the language of the web application, either English (en) or Spanish (es).
  */
 export enum Language {
-    En = "en",
-    Es = "es",
+  En = "en",
+  Es = "es",
 }
 
 /**
  * Theme defines the theme of the web application, either dark or light.
  */
 export enum Theme {
-    Dark = "dark",
-    Light = "light",
+  Dark = "dark",
+  Light = "light",
 }
 
 export interface Status {
-    observedGeneration?: number;
-    phase?:              Phase;
+  observedGeneration?: number;
+  phase?: Phase;
 }
 
 export enum Phase {
-    Failed = "Failed",
-    Pending = "Pending",
-    Ready = "Ready",
+  Failed = "Failed",
+  Pending = "Pending",
+  Ready = "Ready",
 }
 
 RegisterKind(WebApp, {
